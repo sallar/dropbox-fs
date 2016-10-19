65013295aff387c746634efae9ca7d0616cb4461
Salar Kaboli <sallar.kaboli@gmail.com>
2016-10-19 18:37:09 +0300

    feat(module): updated API, module export

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const Dropbox = require("dropbox");
 
-const client = new Dropbox({ accessToken: process.env.KEY });
-
-function __convertStat(entry) {
+/**
+ * Convert an object to fs-like stat object
+ * 
+ * @param {Object} entry
+ * @returns {Object}
+ */
+function __convertToStat(entry) {
     return Object.assign({}, entry, {
         isFile: () => entry['.tag'] === 'file',
         isDirectory: () => entry['.tag'] === 'folder'
     });
 }
 
+/**
+ * Execute a callback async
+ * Borrowed from: https://github.com/perry-mitchell/webdav-fs/blob/master/source/index.js#L19
+ * 
+ * @param {Function} callback
+ * @param {Array.<Any>} args
+ */
 function __executeCallbackAsync(callback, args) {
     if (typeof setImmediate !== "undefined") {
         setImmediate(function() {
@@ -21,148 +32,197 @@ function __executeCallbackAsync(callback, args) {
     }
 }
 
+/**
+ * Normalize an input path string or buffer
+ * Dropbox doesn’t allow '/' for root, it should be an empty string
+ * and some users prefer to prefix the path with a dot.
+ * 
+ * @param {String|Buffer} remotePath
+ * @returns {String}
+ */
 function __normalizePath(remotePath) {
     if (remotePath instanceof Buffer) {
         remotePath = remotePath.toString('utf8');
     }
+
     if (remotePath === '/') {
         return '';
-    } else if (remotePath.indexOf('./') === 0) {
+    }
+    
+    if (remotePath.indexOf('./') === 0) {
         return remotePath.replace(/\.\//, '');
     }
+
     return remotePath;
 }
 
-function readdir(remotePath = '', options = {}, callback) {
-    if (typeof options === 'function') {
-        callback = options;
-        options = {};
+/**
+ * Create an fs-like API for Dropbox
+ * 
+ * @param {{
+ *  apiKey: String,
+ *  client: Dropbox
+ * }} Configuration object
+ * @returns {Object}
+ */
+module.exports = ({apiKey = null, client = null} = {}) => {
+
+    if (!client && typeof apiKey === 'string') {
+        client = new Dropbox({
+            accessToken: apiKey
+        });
+    } else if (!client) {
+        throw new Error('Dropbox client or apiKey should be provided.');
     }
 
-    const mode = options.mode || 'node';
-
-    client
-        .filesListFolder({path: __normalizePath(remotePath)})
-        .then(({entries}) => {
-            if (mode === 'node') {
-                entries = entries.map(entry => entry.name);
-            } else if (mode === 'stat') {
-                entries = entries.map(entry => __convertStat(entry));
-            } else {
-                throw new Error(`Unknow mode: ${mode}`);
+    return {
+        /**
+         * Read a directory and list all the files and folders inside
+         * 
+         * @param {String} remotePath 
+         * @param {Object} options
+         * @param {Function} callback
+         */
+        readdir(remotePath = '', options = {}, callback) {
+            if (typeof options === 'function') {
+                callback = options;
+                options = {};
             }
-            __executeCallbackAsync(callback, [null, entries]);
-        })
-        .catch(callback);
-}
 
-function readFile(remotePath, options = {encoding: null}, callback) {
-    if (typeof options === 'function') {
-        callback = options;
-        options = {
-            encoding: null
-        };
-    } else if (typeof options === 'string') {
-        options = {
-            encoding: options
-        };
+            const mode = options.mode || 'node';
+
+            client
+                .filesListFolder({path: __normalizePath(remotePath)})
+                .then(({entries}) => {
+                    if (mode === 'node') {
+                        entries = entries.map(entry => entry.name);
+                    } else if (mode === 'stat') {
+                        entries = entries.map(entry => __convertToStat(entry));
+                    } else {
+                        throw new Error(`Unknow mode: ${mode}`);
+                    }
+                    __executeCallbackAsync(callback, [null, entries]);
+                })
+                .catch(callback);
+        },
+
+        /**
+         * Read a remote file and return it’s contents
+         * 
+         * @param {String} remotePath
+         * @param {Object} options
+         * @param {Function} callback
+         */
+        readFile(remotePath, options = {encoding: null}, callback) {
+            if (typeof options === 'function') {
+                callback = options;
+                options = {
+                    encoding: null
+                };
+            } else if (typeof options === 'string') {
+                options = {
+                    encoding: options
+                };
+            }
+
+            const { encoding } = options;
+
+            client
+                .filesDownload({path: __normalizePath(remotePath)})
+                .then(resp => {
+                    let buffer = Buffer.from(resp.fileBinary);
+                    buffer = encoding ? buffer.toString(encoding) : buffer;
+                    __executeCallbackAsync(callback, [null, buffer]);
+                })
+                .catch(callback);
+        },
+
+        /**
+         * Rename (move) a remote file
+         * 
+         * @param {String} fromPath
+         * @param {String} toPath
+         * @param {Function} callback
+         */
+        rename(fromPath, toPath, callback) {
+            client
+                .filesMove({
+                    from_path: __normalizePath(fromPath),
+                    to_path: __normalizePath(toPath)
+                })
+                .then(() => {
+                    __executeCallbackAsync(callback, [null]);
+                })
+                .catch(callback);
+        },
+
+        /**
+         * Return file or folder meta data
+         * 
+         * @param {String} remotePath
+         * @param {Function} callback
+         */
+        stat(remotePath, callback) {
+            client
+                .filesGetMetadata({path: __normalizePath(remotePath)})
+                .then(meta => {
+                    meta = __convertToStat(meta);
+                    __executeCallbackAsync(callback, [null, meta]);
+                })
+                .catch(callback);
+        },
+
+        /**
+         * Delete a file or folder
+         * 
+         * @param {String} remotePath
+         * @param {Function} callback
+         */
+        unlink(remotePath, callback) {
+            client
+                .filesDelete({path: __normalizePath(remotePath)})
+                .then(() => {
+                    __executeCallbackAsync(callback, [null]);
+                })
+                .catch(callback);
+        },
+
+        /**
+         * Write a file
+         * 
+         * @param {String} remotePath
+         * @param {String|Buffer} data
+         * @param {Object} options
+         * @param {Function} callback
+         */
+        writeFile(remotePath, data, options = {}, callback) {
+            if (typeof options === 'function') {
+                callback = options;
+                options = {};
+            }
+
+            options = Object.assign({
+                overwrite: true,
+                encoding: 'utf8'
+            }, options);
+
+            const uploadOpts = {
+                path: __normalizePath(remotePath),
+                contents: (data instanceof Buffer) ? data : Buffer.from(data, options.encoding)
+            };
+
+            if (options.overwrite !== false) {
+                uploadOpts.mode = {
+                    '.tag': 'overwrite'
+                };
+            }
+
+            client
+                .filesUpload(uploadOpts).then(meta => {
+                    meta = __convertToStat(meta);
+                    __executeCallbackAsync(callback, [null, meta]);
+                })
+                .catch(callback);
+        }
     }
-
-    const { encoding } = options;
-
-    client
-        .filesDownload({path: __normalizePath(remotePath)})
-        .then(resp => {
-            let buffer = Buffer.from(resp.fileBinary);
-            buffer = encoding ? buffer.toString(encoding) : buffer;
-            __executeCallbackAsync(callback, [null, buffer]);
-        })
-        .catch(callback);
-}
-
-function rename(fromPath, toPath, callback) {
-    client
-        .filesMove({
-            from_path: __normalizePath(fromPath),
-            to_path: __normalizePath(toPath)
-        })
-        .then(() => {
-            __executeCallbackAsync(callback, [null]);
-        })
-        .catch(callback);
-}
-
-function stat(remotePath, callback) {
-    client
-        .filesGetMetadata({path: __normalizePath(remotePath)})
-        .then(meta => {
-            meta = __convertStat(meta);
-            __executeCallbackAsync(callback, [null, meta]);
-        })
-        .catch(callback);
-}
-
-function unlink(remotePath, callback) {
-    client
-        .filesDelete({path: __normalizePath(remotePath)})
-        .then(() => {
-            __executeCallbackAsync(callback, [null]);
-        })
-        .catch(callback);
-}
-
-function writeFile(remotePath, data, options = {}, callback) {
-    if (typeof options === 'function') {
-        callback = options;
-        options = {};
-    }
-
-    options = Object.assign({
-        overwrite: true,
-        encoding: 'utf8'
-    }, options);
-
-    const uploadOpts = {
-        path: __normalizePath(remotePath),
-        contents: (data instanceof Buffer) ? data : Buffer.from(data, options.encoding)
-    };
-
-    if (options.overwrite !== false) {
-        uploadOpts.mode = {
-            '.tag': 'overwrite'
-        };
-    }
-
-    client
-        .filesUpload(uploadOpts).then(meta => {
-            meta = __convertStat(meta);
-            __executeCallbackAsync(callback, [null, meta]);
-        })
-        .catch(callback);
-}
-
-/*
-readdir('/', (err, files) => {
-    console.log(files);
-});*/
-
-/*readFile('/index.js', 'utf8', (err, file) => {
-    console.log(err);
-    console.log(file);
-});*/
-
-/*stat('/index.js', (err, stat) => {
-    console.log(stat);
-});*/
-
-/*writeFile('/sallar2.txt', "sallar 222", (err, stat) => {
-    console.log(err, stat);
-})*/
-
-/*rename('/sallar2.txt', '/sherry.txt', () => {
-    console.log("SHIT");
-});*/
-
-/*unlink('/sallar.txt', () => {
-    console.log("done");
-});*/
+};
