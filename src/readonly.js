c4b850a85484a74e06c091740fed1a534a7eeae9
Nicholas Edwards <edwardsnjd@googlemail.com>
2018-02-01 14:55:09 +0800

    Add an option for read-only mode
    This wraps the standard FS but ensures that any methods that might
    change data are replaced with a noop version returning an error.

---
+++ src/readonly.js
@@ -0,0 +1,36 @@
+import normalDropboxFS from './index';
+
+// List of API methods that should be prevented in read-only mode
+const dangerousMethods = [
+    'mkdir',
+    'rename',
+    'rmdir',
+    'unlink',
+    'writeFile',
+];
+
+/**
+ * Create a read-only fs-like API for Dropbox
+ * 
+ * @param {{
+ *  apiKey: String,
+ *  client: Dropbox,
+ * }} Configuration object
+ * @returns {Object}
+ */
+export default ({ client, apiKey }) => {
+
+    const api = normalDropboxFS({ client, apiKey });
+
+    const returnError = (method) => (...methodArgs) => {
+        const cb = methodArgs[methodArgs.length - 1];
+        cb(`${method} is not supported in read-only mode`);
+    };
+
+    // Replace dangerous methods with safe ones
+    dangerousMethods.forEach(method => {
+        api[method] = returnError(method);
+    });
+
+    return api;
+};
